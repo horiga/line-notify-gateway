673976e891827daa8a00dd4d0c747f1fc3d8b63e
horiga <h.horiga@gmail.com>
2017-01-27 22:46:15 +0900

    reset

---
+++ gh-webhook.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FILE=$1
+TOKEN=$2
+curl -iks -X POST -H 'Content-Type:application/json' \
+  -H 'X-GitHub-Event: pull_request' \
+  -H 'X-Hub-Signature: hogehoge' \
+  -H 'X-GitHub-Delivery: fugafuga' \
+  http://localhost:18081/v1/ghe/payload?notify_token=${TOKEN} --data "@$FILE"
